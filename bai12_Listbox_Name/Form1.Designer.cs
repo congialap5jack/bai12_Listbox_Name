1ea703da3eb0c22f89c2a7fc4a6cdc2652835b69
congialap5jack <kietcuchi148@gmail.com>
2025-03-03 09:02:03 +0700

    22522

--- bai12_Listbox_Name/Form1.Designer.cs
+++ bai12_Listbox_Name/Form1.Designer.cs
@@ -30,9 +30,11 @@
         {
             this.txtName = new System.Windows.Forms.TextBox();
             this.btnADD = new System.Windows.Forms.Button();
-            this.lstNames = new System.Windows.Forms.ListBox();
+            this.lstName = new System.Windows.Forms.ListBox();
             this.btnEDIT = new System.Windows.Forms.Button();
             this.btnREMOVE = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtName
@@ -53,14 +55,14 @@
             this.btnADD.UseVisualStyleBackColor = false;
             this.btnADD.Click += new System.EventHandler(this.btnADD_Click);
             // 
-            // lstNames
+            // lstName
             // 
-            this.lstNames.FormattingEnabled = true;
-            this.lstNames.Location = new System.Drawing.Point(38, 79);
-            this.lstNames.Name = "lstNames";
-            this.lstNames.Size = new System.Drawing.Size(240, 173);
-            this.lstNames.TabIndex = 2;
-            this.lstNames.SelectedIndexChanged += new System.EventHandler(this.lstNames_SelectedIndexChanged);
+            this.lstName.FormattingEnabled = true;
+            this.lstName.Location = new System.Drawing.Point(38, 79);
+            this.lstName.Name = "lstName";
+            this.lstName.Size = new System.Drawing.Size(240, 173);
+            this.lstName.TabIndex = 2;
+            this.lstName.SelectedIndexChanged += new System.EventHandler(this.lstNames_SelectedIndexChanged);
             // 
             // btnEDIT
             // 
@@ -83,13 +85,35 @@
             this.btnREMOVE.Text = "REMOVE";
             this.btnREMOVE.UseVisualStyleBackColor = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(534, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "THÊM VÀO LIST";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(534, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "THÊM VÀO LISTBOX";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lstNames);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstName);
             this.Controls.Add(this.btnREMOVE);
             this.Controls.Add(this.btnEDIT);
             this.Controls.Add(this.btnADD);
@@ -106,8 +130,10 @@
 
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Button btnADD;
-        private System.Windows.Forms.ListBox lstNames;
+        private System.Windows.Forms.ListBox lstName;
         private System.Windows.Forms.Button btnEDIT;
         private System.Windows.Forms.Button btnREMOVE;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- bai12_Listbox_Name/Form1.cs
+++ bai12_Listbox_Name/Form1.cs
@@ -16,10 +16,11 @@ namespace bai12_Listbox_Name
         {
             InitializeComponent();
         }
+        List<string> ten = new List<string>();
 
         private void btnADD_Click(object sender, EventArgs e)
         {
-            lstNames.Items.Add(txtName.Text);
+            lstName.Items.Add(txtName.Text);
             txtName.Text = "";
             txtName.Focus();
 
@@ -27,8 +28,8 @@ namespace bai12_Listbox_Name
 
         private void btnEDIT_Click(object sender, EventArgs e)
         {
-            if (lstNames.SelectedIndex < 0)
-                lstNames.Items[lstNames.SelectedIndex] = txtName.Text;
+            if (lstName.SelectedIndex < 0)
+                lstName.Items[lstName.SelectedIndex] = txtName.Text;
             MessageBox.Show("Hãy chọn tên trong danh sách", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Question);
             return;
         }
@@ -36,13 +37,13 @@ namespace bai12_Listbox_Name
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            lstNames.Items.Add("Trần Văn A");
-            lstNames.Items.Add("Trần Văn B");
-            lstNames.Items.Add("Trần Văn C");
-            lstNames.Items.Add("Trần Văn D");
-            lstNames.Items.Add("Trần Văn E");
-            lstNames.Items.Add("Trần Văn F");
-            lstNames.Items.Add("Trần Văn G");
+            lstName.Items.Add("Trần Văn A");
+            lstName.Items.Add("Trần Văn B");
+            lstName.Items.Add("Trần Văn C");
+            lstName.Items.Add("Trần Văn D");
+            lstName.Items.Add("Trần Văn E");
+            lstName.Items.Add("Trần Văn F");
+            lstName.Items.Add("Trần Văn G");
         }
 
         private void lstNames_SelectedIndexChanged(object sender, EventArgs e)
@@ -51,7 +52,21 @@ namespace bai12_Listbox_Name
             {
                 return;
             }
-            txtName.Text = lstNames.SelectedIndex.ToString();
+            txtName.Text = lstName.SelectedIndex.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ten.Add(txtName.Text);
+            ////
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (string x in ten)
+
+                lstName.Items.Add(x);
+            // Thêm ghi chú
         }
     }
 }
